5da8b36a24086d322f6e2fdb9190fe9749f9005c
Poy Chang <poypost@gmail.com>
2021-07-13 04:38:10 +0800

    add promote

--- webpack/webpack.chrome.js
+++ webpack/webpack.chrome.js
@@ -16,6 +16,10 @@ module.exports = merge(common, {
         },
     },
     plugins: [
+        // copy icons
+        new CopyPlugin([{ from: '.', to: '../app' }], {
+            context: 'src/promote',
+        }),
         // copy manifest
         new CopyPlugin([{ from: './manifest-chrome.json', to: '../manifest.json' }], {
             context: 'src/manifest',
